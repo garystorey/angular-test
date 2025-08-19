6993f09ea63b8392125a13498a935c64601d3611
Gary Storey <garystorey655@gmail.com>
2025-08-19 17:02:32 -0400

    update post list css

--- src/app/shared/post/post.ts
+++ src/app/shared/post/post.ts
@@ -1,9 +1,11 @@
 import { Component, input } from '@angular/core';
+import { SlicePipe } from '@angular/common';
 import { Post, User } from '../../types';
 
 @Component({
   selector: 'app-post',
   standalone: true,
+  imports: [SlicePipe],
   templateUrl: './post.html',
   styleUrl: './post.css',
 })
